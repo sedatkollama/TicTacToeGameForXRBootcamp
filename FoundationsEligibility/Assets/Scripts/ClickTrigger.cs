6c84a9c81e8da194f7b1349792155ac9449bc0bd
Sedat Kollama <sedat.kollama@hotmail.com>
2022-09-15 15:37:21 +0300

    TicTacToe Game
    It was done AI player algorithm, game flow and who win or tie codes.

---
+++ FoundationsEligibility/Assets/Scripts/ClickTrigger.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTrigger : MonoBehaviour
+{
+	TicTacToeAI _ai;
+
+	EndMessage end = new EndMessage();
+
+	int sayac = 0;
+	int artis = 0;
+	int tempCoordX = 0;
+	int tempCoordY = 0;
+	bool yatay = true;
+	bool dikey = true;
+
+	[SerializeField]
+	private int _myCoordX = 0;
+	[SerializeField]
+	private int _myCoordY = 0;
+
+	[SerializeField]
+	private bool canClick;
+
+	private void Awake()
+	{
+		_ai = FindObjectOfType<TicTacToeAI>();
+	}
+
+	// - 3
+	private void Start(){
+		
+		_ai.onGameStarted.AddListener(AddReference);
+		_ai.onGameStarted.AddListener(() => SetInputEndabled(true));
+		_ai.onPlayerWin.AddListener((win) => SetInputEndabled(false));
+		_ai.onPlayerWin.AddListener(end.OnGameEnded);
+	}
+
+	//tiklandiysa true yapılıyor
+	private void SetInputEndabled(bool val){
+		canClick = val;
+	}
+
+	// - 4 
+	private void AddReference()
+	{
+		_ai.RegisterTransform(_myCoordX, _myCoordY, this);
+		canClick = true;
+	}
+
+	private void OnMouseDown()
+	{
+		if(canClick){
+			_ai.PlayerSelects(_myCoordX, _myCoordY);
+			canClick = false;//sonradan ekledim					
+            if (_ai.turning == 9 && !_ai.won)
+            {
+				_ai.Winning(TicTacToeState.none);
+            }
+            if (!_ai.won)
+            {
+				Invoke("AiPlaying", 0.5f);//Ai oynayacak
+			}			
+		}
+	}
+
+	void _AiRandom()
+    {
+        if (_ai._triggers[1, 1].canClick == true)
+        {
+			_ai.AiSelects(1, 1);
+			_ai._triggers[1, 1].canClick = false;
+        }
+        else
+        {
+			for (int i = 0; i < 3; i++)
+			{
+				for (int z = 0; z < 3; z++)
+				{
+					if (_ai._triggers[i, z].canClick == true && !_ai._isPlayerTurn)
+					{
+						_ai.AiSelects(i, z);
+						_ai._triggers[i, z].canClick = false;
+						break;
+					}
+				}
+				if (_ai._isPlayerTurn)
+				{
+					break;
+				}
+			}
+		}		
+	}
+	void AiPlaying()
+	{
+		if (_ai.turning <= 2)
+		{
+			_AiRandom();
+		}
+		//yatay kontrol
+		if (_ai.turning > 2)
+		{
+			sayac = 0;
+			for (int i = 0; i < 3; i++)
+			{
+				for (int z = 0; z < 3; z++)
+				{
+					if (_ai.boardState[i, z] == TicTacToeState.circle && !_ai._isPlayerTurn)
+					{
+						sayac++;
+					}
+					if (_ai._triggers[i, z].canClick == true && !_ai._isPlayerTurn)
+					{
+						tempCoordX = i;
+						tempCoordY = z;
+					}
+					if (_ai._triggers[i, z].canClick == false && !_ai._isPlayerTurn)
+					{
+						artis++;
+					}
+					if (sayac == 2)
+					{
+						if (artis == 3)
+						{
+							break;
+						}
+						else
+						{
+							if (z == 1)
+							{
+								//dikey kontrole yonlendirme yapmalıyım
+								if (_ai._triggers[i, z + 1].canClick == false)
+								{
+									//_AiRandom();
+									break;//bu satirda iki tane O var ama, bosluk yok.
+								}
+								_ai.AiSelects(i, z + 1);
+								_ai._triggers[i, z + 1].canClick = false;
+								yatay = false;
+								break;
+							}
+							else
+							{
+								_ai.AiSelects(tempCoordX, tempCoordY);
+								_ai._triggers[tempCoordX, tempCoordY].canClick = false;
+								yatay = false;
+								break;
+							}
+						}
+					}
+				}
+				sayac = 0;
+				artis = 0;
+				if (_ai._isPlayerTurn)
+				{
+					break;
+				}
+			}			
+		}
+		//dikey kontrol
+		if (_ai.turning > 2 && !_ai._isPlayerTurn)
+		{
+			sayac = 0;
+			if (yatay)
+			{
+				for (int z = 0; z < 3; z++)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						if (_ai.boardState[i, z] == TicTacToeState.circle && !_ai._isPlayerTurn)
+						{
+							sayac++;
+						}
+						if (_ai._triggers[i, z].canClick == true && !_ai._isPlayerTurn)
+						{
+							tempCoordX = i;
+							tempCoordY = z;
+						}
+						if (_ai._triggers[i, z].canClick == false && !_ai._isPlayerTurn)
+						{
+							artis++;
+						}
+						if (sayac == 2)
+						{
+							if (artis == 3)
+							{
+								break;
+							}
+							else
+							{
+								if (i == 1 && sayac == 2)
+								{
+									//capraz kontrole yonlendirme yapmalıyım
+									if (_ai._triggers[i + 1, z].canClick == false)
+									{
+										//_AiRandom();
+										break;//bu sütunda iki tane O var ama, bosluk yok.
+									}
+									_ai.AiSelects(i + 1, z);
+									_ai._triggers[i + 1, z].canClick = false;
+									yatay = false;
+									break;
+								}
+								else
+								{
+									_ai.AiSelects(tempCoordX, tempCoordY);
+									_ai._triggers[tempCoordX, tempCoordY].canClick = false;
+									yatay = false;
+									break;
+								}
+							}
+						}
+					}
+					sayac = 0;
+					artis = 0;
+					if (_ai._isPlayerTurn)
+					{
+						break;
+					}
+				}
+			}
+		}
+		//capraz kontrol
+		if (_ai.turning > 2 && !_ai._isPlayerTurn)
+		{
+			sayac = 0;
+			if (dikey)
+			{
+				if (_ai.boardState[0, 0] == TicTacToeState.circle && _ai.boardState[1, 1] == TicTacToeState.circle && !_ai._isPlayerTurn)
+				{
+					sayac++;
+				}
+				if (_ai.boardState[0, 0] == TicTacToeState.circle && _ai.boardState[2, 2] == TicTacToeState.circle && !_ai._isPlayerTurn)
+				{
+					sayac++;
+				}
+				if (_ai.boardState[1, 1] == TicTacToeState.circle && _ai.boardState[2, 2] == TicTacToeState.circle && !_ai._isPlayerTurn)
+				{
+					sayac++;
+				}
+				if (sayac == 1)
+				{
+					for (int i = 0; i < 3; i++)
+					{
+						if (_ai._triggers[i, i].canClick == false && !_ai._isPlayerTurn)
+						{
+							artis++;
+						}
+						if (_ai._triggers[i, i].canClick == true && !_ai._isPlayerTurn)
+						{
+							tempCoordX = i;
+							tempCoordY = i;
+							break;
+						}					
+					}
+					if (artis == 3)
+					{
+						//bir sey yapma
+					}
+					else
+					{
+						_ai.AiSelects(tempCoordX, tempCoordY);
+						_ai._triggers[tempCoordX, tempCoordY].canClick = false;
+						yatay = false;
+					}
+					artis = 0;
+					sayac = 0;
+				}
+			}
+            //diger taraf           
+            if (_ai.boardState[0, 2] == TicTacToeState.circle && _ai.boardState[1, 1] == TicTacToeState.circle && !_ai._isPlayerTurn)
+            {
+                sayac++;
+            }
+            if (_ai.boardState[0, 2] == TicTacToeState.circle && _ai.boardState[2, 0] == TicTacToeState.circle && !_ai._isPlayerTurn)
+            {
+                sayac++;
+            }
+            if (_ai.boardState[2, 0] == TicTacToeState.circle && _ai.boardState[1, 1] == TicTacToeState.circle && !_ai._isPlayerTurn)
+            {
+                sayac++;
+            }
+            if (sayac == 1)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+					if (_ai._triggers[i, 2-i].canClick == false && !_ai._isPlayerTurn)
+					{
+						artis++;
+					}
+					if (_ai._triggers[i, 2-i].canClick == true && !_ai._isPlayerTurn)
+                    {
+                        tempCoordX = i;
+                        tempCoordY = 2 - i;
+                        break;
+                    }
+                }
+				if (artis == 3)
+				{
+					//bir sey yapma
+				}
+				else
+				{
+					_ai.AiSelects(tempCoordX, tempCoordY);
+					_ai._triggers[tempCoordX, tempCoordY].canClick = false;
+					yatay = false;
+				}
+				artis = 0;
+				sayac = 0;				
+            }
+        }
+        if (!_ai._isPlayerTurn)
+        {
+			_AiRandom();
+		}		
+    }
+}
---
+++ FoundationsEligibility/Assets/Scripts/TicTacToeAI.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum TicTacToeState{none = -1, cross = 1, circle = 2} // sayi ekledim
+
+[System.Serializable]
+public class WinnerEvent : UnityEvent<int>
+{
+	
+}
+
+public class TicTacToeAI : MonoBehaviour
+{
+	public int turning = 0;
+	int _aiLevel;
+	public bool won = false;
+
+	public TicTacToeState[,] boardState;
+
+	[SerializeField]
+	public bool _isPlayerTurn;
+
+    [SerializeField]
+    private int _gridSize = 3;
+
+    [SerializeField]
+	private TicTacToeState playerState = TicTacToeState.circle;
+	TicTacToeState aiState = TicTacToeState.cross;
+
+	[SerializeField]
+	private GameObject _xPrefab;
+
+	[SerializeField]
+	private GameObject _oPrefab;
+
+	public UnityEvent onGameStarted;
+
+	//Call This event with the player number to denote the winner
+	public WinnerEvent onPlayerWin;
+
+	public ClickTrigger[,] _triggers;
+	
+	private void Awake()
+	{
+		if(onPlayerWin == null){
+			onPlayerWin = new WinnerEvent();
+		}
+		boardState = new TicTacToeState[3,3];
+	}
+
+	public void StartAI(int AILevel){
+		_aiLevel = AILevel;
+		StartGame();
+	}
+
+	public void RegisterTransform(int myCoordX, int myCoordY, ClickTrigger clickTrigger)
+	{
+		_triggers[myCoordX, myCoordY] = clickTrigger;
+	}
+
+	private void StartGame()
+	{
+		_triggers = new ClickTrigger[3,3];
+		turning = 0;
+		onGameStarted.Invoke();
+	}
+
+	public void PlayerSelects(int coordX, int coordY){
+
+		SetVisual(coordX, coordY, playerState);
+		turning++;
+		_isPlayerTurn = false;
+	}
+
+	public void AiSelects(int coordX, int coordY){
+
+		SetVisual(coordX, coordY, aiState);
+		turning++;
+		_isPlayerTurn = true;
+	}
+
+	private void SetVisual(int coordX, int coordY, TicTacToeState targetState)
+	{	
+		Instantiate(
+			targetState == TicTacToeState.circle ? _oPrefab : _xPrefab,
+			_triggers[coordX, coordY].transform.position,
+			Quaternion.identity
+		);
+		//sonradan ekledim
+		boardState[coordX, coordY] = targetState;
+		CheckWinner(targetState);
+	}
+
+	void CheckWinner(TicTacToeState targetState)
+    {
+		switch (targetState)
+		{
+			case TicTacToeState.circle:
+				CheckInstructions(targetState);
+				break;
+			case TicTacToeState.cross:
+				CheckInstructions(targetState);
+				break;
+		}
+	}
+
+    void CheckInstructions(TicTacToeState target)
+    {
+		//capraz kazanma
+        if (boardState[0,0] == target && boardState[1,1] == target && boardState[2,2] == target)
+        {			
+			won = true;
+			Winning(target);
+		}
+		else if (boardState[2, 0] == target && boardState[1, 1] == target && boardState[0, 2] == target)
+        {
+			won = true;
+			Winning(target);
+		}
+		//dikey kazanma
+		else if (boardState[0, 0] == target && boardState[1, 0] == target && boardState[2, 0] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+		else if (boardState[0, 1] == target && boardState[1, 1] == target && boardState[2, 1] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+		else if (boardState[0, 2] == target && boardState[1, 2] == target && boardState[2, 2] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+		//yatay kazanma
+		else if (boardState[0, 0] == target && boardState[0, 1] == target && boardState[0, 2] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+		else if (boardState[1, 0] == target && boardState[1, 1] == target && boardState[1, 2] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+		else if (boardState[2, 0] == target && boardState[2, 1] == target && boardState[2, 2] == target)
+		{
+			won = true;
+			Winning(target);
+		}
+	}
+
+	public void Winning(TicTacToeState target)
+    {
+		if(turning == 9 && !won)
+        {
+			onPlayerWin.Invoke((int)TicTacToeState.none);
+		}			
+		onPlayerWin.Invoke((int)target);
+	}
+}
